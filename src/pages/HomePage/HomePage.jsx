366aa2457da9e3d8297dec0ed8e1b295301f6ea3
Ian Cha <chaheein@gmail.com>
2020-07-25 09:08:15 -0700

    continuing set up

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -18,7 +18,21 @@ export default class HomePage extends React.Component {
   render() {
     return (
       <div className="HomePage">
-        
+        <Container>
+          <Row>
+            <Col xs={{ order: 0, span: 12}} md={{ order: 0, span: 4}} lg={3} className="homecols">
+              first
+            </Col>
+
+            <Col xs={{ order: 2, span: 12}} md={{ order: 1, span: 8}} lg={6} className="homecols">
+              second
+            </Col>
+
+            <Col xs={{ order: 1, span: 12}} md={{ order: 2, span: 4}} lg={3} className="home-right homecols">
+              third
+            </Col>
+          </Row>
+        </Container>
         
       </div>
     )
