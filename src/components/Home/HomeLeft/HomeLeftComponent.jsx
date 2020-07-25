2348980423c6f1e3e2ccde9192c5c28ae7f078e5
Ian Cha <chaheein@gmail.com>
2020-07-25 11:19:33 -0700

    need to now fix the header

--- src/components/Home/HomeLeft/HomeLeftComponent.jsx
+++ src/components/Home/HomeLeft/HomeLeftComponent.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import {top10tags} from "../../../data/dummydata";
 
 import "./HomeLeftComponent.scss";
 
 const HomeLeftComponent = () => {
   return (
-    <div>
-      home left
+    <div className="HomeLeftComponent">
+      <div className="homeleft-tags">
+        <h5>인기 검색어</h5>
+        {
+          top10tags.map((tag,index) => {
+            return(
+              <div key={index}>
+                #{tag}
+              </div>
+            )
+          })
+        }
+
+
+      </div>
+
+      <div className="homeleft-top-posts">
+
+
+
+      </div>
     </div>
   )
 }
